d2ca98ebd4d61affb3e73fb677b9e32b627b452d
obusandy <sandraobunga29@gmail.com>
2024-10-06 14:14:33 +0300

    Donate API

--- front-end/src/components/Buttons.js
+++ front-end/src/components/Buttons.js
@@ -4,10 +4,17 @@ import "./Buttons.css";
 export default function Buttons() {
   return (
     <div className="fixed-buttons btn-group-vertical">
-      <button className="btn btn-danger">
-        <i className="bi bi-flag"></i>Report a case
+      <button className="btn btn-danger">Report a case</button>
+      <button className="btn btn-primary">
+        <a
+          href="https://donate.stripe.com/test_7sI7tY5sv1U2c2k9AA"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "black" }}
+        >
+          Donate
+        </a>
       </button>
-      <button className="btn btn-primary">Donate</button>
     </div>
   );
 }
